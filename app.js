70abefd2cc427cd6cf1f2e495bbbdc0353af49ec
matteo_lovato <lovato.teo@gmail.com>
2020-10-31 21:31:04 +0100

    add input slider hue colors background

--- app.js
+++ app.js
@@ -76,6 +76,9 @@ function colorizeSliders(color, hue, brightness, saturation) {
   brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(
     0
   )}, ${scaleBright(0.5)}, ${scaleBright(1)})`;
+
+  //Hue is always the same all the colors
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75), rgb(204,204,75), rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204))`;
 }
 
 randomColors();
