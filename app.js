04d3742fbf0ec5d9cf8f025ba6d5dab5cd10ba7d
matteo_lovato <lovato.teo@gmail.com>
2020-11-02 20:45:20 +0100

    Add sliders change the background color

--- app.js
+++ app.js
@@ -3,7 +3,21 @@ const colorDivs = document.querySelectorAll(".color");
 const generateBtn = document.querySelector(".generate");
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexs = document.querySelectorAll(".color h2");
+// array to save colors
+// otherwise saturation will brake the magic
 let initialColors;
+
+//Event listener
+
+sliders.forEach((slider) => {
+  slider.addEventListener("input", hslControls);
+});
+
+colorDivs.forEach((div, index) => {
+  div.addEventListener("change", () => {
+    updateTextUI(index);
+  });
+});
 
 // functions
 
@@ -25,10 +39,13 @@ function generateHex() {
 }
 
 function randomColors() {
+  // initialize colors array
+  initialColors = [];
   colorDivs.forEach((div, index) => {
     const hexText = div.children[0];
     const randomColor = generateHex();
-
+    // push hex colors to array
+    initialColors.push(chroma(randomColor).hex());
     // add colors to the background
     div.style.backgroundColor = randomColor;
     hexText.innerText = randomColor;
@@ -79,6 +96,39 @@ function colorizeSliders(color, hue, brightness, saturation) {
 
   //Hue is always the same all the colors
   hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75), rgb(204,204,75), rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204), rgb(204,75,75))`;
+}
+
+function hslControls(e) {
+  const index =
+    e.target.getAttribute("data-bright") ||
+    e.target.getAttribute("data-sat") ||
+    e.target.getAttribute("data-hue");
+  let sliders = e.target.parentElement.querySelectorAll('input[type="range"]');
+  const hue = sliders[0];
+  const brightness = sliders[1];
+  const saturation = sliders[2];
+
+  const bgColor = initialColors[index];
+  let color = chroma(bgColor)
+    .set("hsl.s", saturation.value)
+    .set("hsl.l", brightness.value)
+    .set("hsl.h", hue.value);
+
+  colorDivs[index].style.backgroundColor = color;
+}
+
+function updateTextUI(index) {
+  const activeDiv = colorDivs[index];
+  const color = chroma(activeDiv.style.backgroundColor);
+  const textHex = activeDiv.querySelector("h2");
+  const icons = activeDiv.querySelectorAll(".controls button");
+  textHex.innerText = color.hex();
+  //check contrast
+  checkTextContrast(color, textHex);
+  // check contrast of icons
+  for (icon of icons) {
+    checkTextContrast(color, icon);
+  }
 }
 
 randomColors();
