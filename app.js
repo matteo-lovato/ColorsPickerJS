fa148a18a413d7054cbf2299543f2ac7bcf798f8
matteo_lovato <lovato.teo@gmail.com>
2020-11-03 14:41:53 +0100

    add get palette saved from library

--- app.js
+++ app.js
@@ -308,6 +308,21 @@ function savePalette(e) {
   paletteBtn.classList.add("pick-palette-btn");
   paletteBtn.classList.add(paletteObj.nr);
   paletteBtn.innerText = "Select";
+
+  //attach event to the button
+  paletteBtn.addEventListener("click", (e) => {
+    closeLibrary();
+    const paletteIndex = e.target.classList[1];
+    initialColors = [];
+    savedPalettes[paletteIndex].colors.forEach((color, index) => {
+      initialColors.push(color);
+      colorDivs[index].style.backgroundColor = color;
+      const text = colorDivs[index].children[0];
+      updateTextUI(index);
+    });
+    resetInputs();
+  });
+
   // append to library
   palette.appendChild(title);
   palette.appendChild(preview);
